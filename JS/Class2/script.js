f4c04740b65e44830a9103f6d522a2d98c0d52c3
ManojNambiraj <postboxmano@gmail.com>
2025-11-11 20:31:41 +0530

    JS Operators

---
+++ JS/Class2/script.js
@@ -0,0 +1,59 @@
+// Datatypes:
+
+    // let a = 10
+    // let a = `hello`
+    // let a = false
+    // let a
+    // let a = null
+
+    // console.log(a, typeof(a));
+
+// Operator:
+
+    // Arithmetic operators
+        // (+, -, *, /, %, **, ++, --)
+
+            // let a = 5
+            // let b = 2
+            // let result
+
+            // result = a + b
+            // result = a - b
+            // result = a * b
+            // result = a / b
+            // result = a % b
+            // result = a ** b
+            // result = ++a
+            // result = --a
+
+            // console.log(result);
+
+    // Assignment operators
+        // (=, +=, -=, *=, /=, %=, **=)
+
+            // let a = 5
+
+            // a += 2     // a = a + 2
+            // a -= 2     // a = a - 2
+
+            // console.log(a);
+        
+    // Comparison operators --> (true or false) -- Boolean
+        // (==, ===, !=, !==, >, <, >=, <=)
+
+            // let age = 18
+            // let result
+
+            // result = (age == 18)
+            // result = (age === 18)
+            // result = (age !== 18)
+            // result = (age > 18)
+            // result = (age < 18)
+            // result = (age <= 18)
+
+            // console.log(result);
+
+    // Logical operators
+    // Ternary operators
+    // Type operator
+
