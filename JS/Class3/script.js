8ecfdfbaf9659c7dfbf0edbff0d9ea832ae0e546
ManojNambiraj <postboxmano@gmail.com>
2025-11-12 21:15:18 +0530

    OP & Conditional Stmts

---
+++ JS/Class3/script.js
@@ -0,0 +1,111 @@
+// Operators:
+
+    // Logical OP
+        // (&&, ||, !)
+
+        // &&
+
+            // (true)  && (true)  --> true
+            // (true)  && (false) --> false
+            // (false) && (true)  --> false
+            // (false) && (false) --> false
+
+        // ||
+
+            // (true)  || (true)  --> true
+            // (true)  || (false) --> true
+            // (false) || (true)  --> true
+            // (false) || (false) --> false
+
+        // !
+
+            // !(true)  --> false
+            // !(false) --> true
+
+
+        // let age = 32
+        // let result
+
+        // // result = (age > 18) && (age < 30)
+        // result = (age > 18) || (age < 30)
+
+        // console.log(!result)
+        
+    // Ternary OP -> () ? "" : ""
+
+        // let age = 16
+        // let result
+
+        // result = (age > 18) ? "Eligible for voting" : "Not Eligible"
+
+        // console.log(result);
+        
+    // Type OP
+
+        // let a = 10
+
+        // console.log(a,  typeof(a));
+        
+
+// Control Statements:
+
+    // Conditional Stmts
+        // if
+
+            // let age = 16
+
+            // if(age > 18){
+            //     console.log("Eligible for voting");
+            // }
+
+        // if else
+
+            // let age = 16;
+
+            // if (age > 18) {
+            //   console.log("Eligible for voting");
+            // } else {
+            //   console.log("Not Eligible");
+            // }
+
+        // if else if
+
+            // let age = 18;
+
+            // if (age > 18) {
+            //   console.log("Eligible for voting");
+            // } else if(age == 18){
+            //   console.log("Partially Eligible for voting");
+            // } else {
+            //   console.log("Not Eligible");
+            // }
+        
+        // switch case
+
+            // let day = 10
+
+            // switch (day) {
+            //     case 1:
+            //         console.log("It's a Monday");
+            //         break
+            //     case 2:
+            //         console.log("It's a Tuesday");
+            //         break
+            //     case 3:
+            //         console.log("It's a Wednesday");
+            //         break
+            //     case 4:
+            //         console.log("It's a Thursday");
+            //         break
+            //     case 5:
+            //         console.log("It's a Friday");
+            //         break
+            //     case 6:
+            //         console.log("It's a Saturday");
+            //         break
+            //     default:
+            //         console.log("Happy Weekend");
+            // }
+
+    // Looping Stmts
+    // Jumping Stmts
