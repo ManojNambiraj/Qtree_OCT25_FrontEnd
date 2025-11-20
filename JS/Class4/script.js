d6daab4eba6c3e616106f0c931ed19267bbfae4b
ManojNambiraj <postboxmano@gmail.com>
2025-11-20 20:39:41 +0530

    Arrays & Objects

---
+++ JS/Class4/script.js
@@ -0,0 +1,43 @@
+// Control stmts:
+// Conditional stmts
+
+// Looping stmts
+// for loop
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// while loop
+
+// let i = 1
+
+// while (i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+// do-while loop
+
+// let i = 1
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 10)
+
+// Jumping stmts
+
+// break  // continue
+
+let i = 0;
+
+while (i < 5) {
+  i++;
+
+  if (i == 3) {
+    continue;
+  }
+
+  console.log(i);
+}
---
+++ JS/Class5/script.js
@@ -0,0 +1,58 @@
+// Arrays:
+
+    // let numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+    // // index --->  #0  1   2   3   4    5   6...........
+
+    // console.log(numbers[0]);
+    // console.log(numbers[4]);
+
+
+    // numbers[4] = 5000
+
+    // numbers.push(9000)
+    // numbers.pop()
+    // console.log(numbers.length);
+
+    // console.log(numbers);
+
+// Object: {key: value}
+
+    // let student = {name: "Raj", age: 23, mobile: 987654321, email: "Raj@gmail.com"}
+
+    // console.log(student.name);
+    // console.log(student.age);
+
+    // student.name = "Rajkumar"
+    // student.age = 35
+    // student.dept = "XYZ"
+
+    // delete student.name
+
+    // console.log(student);
+
+// Destructuring:
+
+    // Array Destructuring
+
+        // let fruits = ["apple", "banana", "orange", "grapes"]
+
+        // const [red, oran, black] = fruits
+
+        // console.log(red);
+        // console.log(oran);
+
+    // Object Destructuring
+
+        // let student = {
+        // name: "Raj",
+        // age: 23,
+        // mobile: 987654321,
+        // email: "Raj@gmail.com",
+        // };
+
+        // const {name, email, age} = student
+
+        // console.log(name);
+        // console.log(age);
+
+
