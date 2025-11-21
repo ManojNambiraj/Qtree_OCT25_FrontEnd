7b207741db1676bd4680e30cd9a89ede1ef8eca8
ManojNambiraj <postboxmano@gmail.com>
2025-11-21 20:21:15 +0530

    Functions

---
+++ JS/Class6/script.js
@@ -0,0 +1,31 @@
+// Function: --> DRY
+
+// Normal function
+
+    // function demo(a, b){                     // Func def
+    //     return a * b
+    // }
+
+    // const result = demo(4, 200)              // func Call
+
+    // console.log(result);
+
+// Ananymous Function --> (Unnamed functions)
+
+    // const demo = function(){
+    //     console.log("hello");
+    // }
+
+    // demo()
+
+// Arrow function
+
+    // const arr = (a, b) => a * b;
+
+    // console.log(arr(2, 5));
+
+// IIFE Function: (Immediately Invoked Function Expression)
+
+    // (() => {
+    //     console.log("I'm IIFE");
+    // })()
