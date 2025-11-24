19a5eb828a4b4358cd719eaae6f10a10982ac54c
ManojNambiraj <postboxmano@gmail.com>
2025-11-24 20:07:41 +0530

    MRF Methods

---
+++ JS/Class7/script.js
@@ -0,0 +1,46 @@
+// Arry methods:
+
+    // MRF --> (Map, Reduce, Filter)
+
+        // let fruits = ["apple", "orange", "banana", "grapes"]
+
+        // for(let i = 0; i < fruits.length; i++){
+        //     console.log(fruits[i]);
+        // }
+
+// Map:
+
+    //  fruits.map((item) => console.log(item));
+
+    // let numbers = [2, 4, 7, 9, 11, 24, 37, 5, 10, 12]
+
+    // let result = numbers.map((item) => {
+    //     return item * 3
+    // })
+
+    // console.log(result);
+
+// Filter:
+
+    // let numbers = [2, 4, 7, 9, 11, 24, 37, 5, 10, 12];
+
+    // let result = numbers.filter((item) => {
+    //     return item % 2 != 0
+    // })
+
+    // console.log(result);
+
+
+// Reduce
+
+    // let numbers = [2, 4, 7, 9, 11, 24, 37, 5, 10, 12];
+
+    // let result = numbers.reduce((prev, curr)=>{
+    //     return prev + curr
+    // })
+
+    // console.log(result);
+
+
+   
+
