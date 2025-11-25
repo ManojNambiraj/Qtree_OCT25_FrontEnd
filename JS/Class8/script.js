547f9587250bc809ce52bc93bb2997d8d352be4e
ManojNambiraj <postboxmano@gmail.com>
2025-11-25 21:14:26 +0530

    JS DOM

---
+++ JS/Class8/script.js
@@ -0,0 +1,45 @@
+// DOM --> Document Object Model
+
+    // let ele = document.getElementById("demo")
+
+    // ele.innerText = "Good Morning... John...!"
+
+    // ele.innerHTML = "<h1>Good Morning... John...</h1>";
+
+    // let ele = document.getElementsByClassName("demo")
+
+    // console.log(ele);
+
+    // ele[0].innerHTML = "hello"
+    // ele[1].innerHTML = "hello"
+
+    // let ele = document.getElementsByTagName("p")
+
+    // console.log(ele);
+
+    // ele[1].innerHTML = "hello"
+
+    // let ele = document.getElementById("demo");
+
+    // ele.innerText = "Good Morning... John...!";
+    // ele.style.color = "orange"
+    // ele.style.fontSize = "50px"
+
+// Create Element
+
+    // let newDiv = document.createElement("ul")
+
+    // let newEle = document.createElement("li")
+
+    // newEle.setAttribute("id", "heading")
+
+    // newEle.innerHTML = "Hello world"
+
+    // document.body.append(newDiv);
+    // newDiv.appendChild(newEle)
+
+    // let ele = document.querySelector("h1")
+
+    // console.log(ele);
+
+    // ele.innerText = "Hello"
